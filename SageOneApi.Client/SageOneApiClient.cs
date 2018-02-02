d83d3850267f64a7064e65e3d9ed65f90035c088
Jonathan Percy <jonpercydev@gmail.com>
2018-02-02 11:06:31 +0000

    do not make user of API client have to wrap the handlers

---
+++ SageOneApi.Client/SageOneApiClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SageOneApi.Client
+{
+    public class SageOneApiClient : SageOneApiClientBaseHandler
+    {
+        public SageOneApiClient(
+            Uri baseUri,
+            string accessToken,
+            string subscriptionId,
+            string resourceOwnerId,
+            Func<string> renewRefreshAndAccessToken) : base(
+                new SageOneApiClientPagingHandler(
+                    new SageOneApiClientExceptionHandler(
+                        new SageOneApiClientTransferHandler(baseUri, accessToken, subscriptionId, resourceOwnerId, renewRefreshAndAccessToken))))
+        { }
+    }
+}
