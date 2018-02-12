5e371ffd6bdb96b1aed7f01cb0025bf5c3aff02c
Jonathan Percy <jonpercydev@gmail.com>
2018-02-12 16:14:21 +0000

    purchase credit note needs to inherit from purchase transaction

--- SageOneApi.Client/Models/PurchaseCreditNote.cs
+++ SageOneApi.Client/Models/PurchaseCreditNote.cs
@@ -2,7 +2,7 @@
 
 namespace SageOneApi.Client.Models
 {
-    public class PurchaseCreditNote
+    public class PurchaseCreditNote : PurchaseTransaction
     {
         public List<PurchaseTransactionLine> credit_note_lines { get; set; }
     }
