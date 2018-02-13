276d342b50190689035959d4d245441dcad8fe4e
Jonathan Percy <jonpercydev@gmail.com>
2018-02-13 12:23:45 +0000

    add TransactionTypes

---
+++ SageOneApi.Client/Constants/TransactionType.cs
@@ -0,0 +1,14 @@
+﻿namespace SageOneApi.Client.Constants
+{
+    public static class TransactionType
+    {
+        public const string SalesInvoice = "SALES_INVOICE";
+        public const string SalesCreditNote = "SALES_CREDIT_NOTE";
+        public const string PurchaseInvoice = "PURCHASE_INVOICE";
+        public const string PurchaseCreditNote = "PURCHASE_CREDIT_NOTE";
+        public const string CustomerReceipt = "SALES_RECEIPT";
+        public const string CustomerRefund = "CUSTOMER_REFUND";
+        public const string VendorPayment = "VENDOR_PAYMENT";
+        public const string VendorRefund = "VENDOR_REFUND";
+    }
+}
