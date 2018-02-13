ba0431e9b310df399e1e16eaa524d54ff0c73eba
Jonathan Percy <jonpercydev@gmail.com>
2018-02-13 14:22:17 +0000

    fix incorrect transaction type

--- SageOneApi.Client/Constants/TransactionType.cs
+++ SageOneApi.Client/Constants/TransactionType.cs
@@ -6,7 +6,7 @@
         public const string SalesCreditNote = "SALES_CREDIT_NOTE";
         public const string PurchaseInvoice = "PURCHASE_INVOICE";
         public const string PurchaseCreditNote = "PURCHASE_CREDIT_NOTE";
-        public const string CustomerReceipt = "SALES_RECEIPT";
+        public const string CustomerReceipt = "CUSTOMER_RECEIPT";
         public const string CustomerRefund = "CUSTOMER_REFUND";
         public const string VendorPayment = "VENDOR_PAYMENT";
         public const string VendorRefund = "VENDOR_REFUND";
