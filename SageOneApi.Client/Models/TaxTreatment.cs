884805c9aef13a0181b9e726ff279b604838877b
Jonathan Percy <jonpercydev@gmail.com>
2018-04-18 15:16:56 +0100

    make properties nullable

--- SageOneApi.Client/Models/TaxTreatment.cs
+++ SageOneApi.Client/Models/TaxTreatment.cs
@@ -3,8 +3,8 @@
     public class TaxTreatment
     {
         public bool home_tax { get; set; }
-        public bool eu_tax_registered { get; set; }
-        public bool eu_not_tax_registered { get; set; }
+        public bool? eu_tax_registered { get; set; }
+        public bool? eu_not_tax_registered { get; set; }
         public bool rest_of_world_tax { get; set; }
     }
 }
