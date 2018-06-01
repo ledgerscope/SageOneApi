d34e5b08bffb5df4e73df02f66dd12bc1c8a9103
Matthew Steeples <matthew@mercuryit.co.uk>
2018-06-01 18:42:29 +0100

    Add BusinessSettings to the retrievable list of entities

---
+++ SageOneApi.Client/Models/BusinessSettings.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageOneApi.Client.Models
+{
+    public class BusinessSettings
+    {
+        [JsonProperty("$path")]
+        public string path { get; set; }
+
+        public PropertyValueWithPath country_of_registration { get; set; }
+
+        public DefaultLedgerAccounts default_ledger_accounts { get; set; }
+    }
+}
---
+++ SageOneApi.Client/Models/DefaultLedgerAccounts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageOneApi.Client.Models
+{
+    public class DefaultLedgerAccounts
+    {
+        public PropertyValueWithPath bank_charges_ledger_account { get; set; }
+        public PropertyValueWithPath bank_interest_charges_paid_ledger_account { get; set; }
+        public PropertyValueWithPath bank_interest_received_ledger_account { get; set; }
+        public PropertyValueWithPath carriage_ledger_account { get; set; }
+        public PropertyValueWithPath customer_receipt_discount_ledger_account { get; set; }
+        public PropertyValueWithPath exchange_rate_gains_ledger_account { get; set; }
+        public PropertyValueWithPath exchange_rate_losses_ledger_account { get; set; }
+        public PropertyValueWithPath other_payment_ledger_account { get; set; }
+        public PropertyValueWithPath other_receipt_ledger_account { get; set; }
+        public PropertyValueWithPath product_purchase_ledger_account { get; set; }
+        public PropertyValueWithPath product_sales_ledger_account { get; set; }
+        public PropertyValueWithPath purchase_discount_ledger_account { get; set; }
+        public PropertyValueWithPath purchase_ledger_account { get; set; }
+        public PropertyValueWithPath sales_discount_ledger_account { get; set; }
+        public PropertyValueWithPath sales_ledger_account { get; set; }
+        public PropertyValueWithPath service_sales_ledger_account { get; set; }
+        public PropertyValueWithPath vendor_payment_discount_ledger_account { get; set; }
+    }
+}
