7b60cb8aa1e14f2b419cc1ace56204de7be0f2ee
Matthew Steeples <matthew@mercuryit.co.uk>
2018-07-08 20:55:13 +0100

    Make dates optional

--- SageOneApi.Client/Models/SageOneEntity.cs
+++ SageOneApi.Client/Models/SageOneEntity.cs
@@ -9,7 +9,7 @@ namespace SageOneApi.Client.Models
         public string path { get; set; }
         public string id { get; set; }
         public string displayed_as { get; set; }
-        public DateTime created_at { get; set; }
-        public DateTime updated_at { get; set; }
+        public DateTime? created_at { get; set; }
+        public DateTime? updated_at { get; set; }
     }
 }
