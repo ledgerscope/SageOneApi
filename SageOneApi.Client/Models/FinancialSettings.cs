a24de601c2aab6a4cbe48c45e62bbaa54c51afd4
Matthew Steeples <matthew@ledgerscope.com>
2019-04-29 10:46:56 +0100

    Add some missing properties (MTD related)

--- SageOneApi.Client/Models/FinancialSettings.cs
+++ SageOneApi.Client/Models/FinancialSettings.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace SageOneApi.Client.Models
 {
@@ -18,11 +19,19 @@ namespace SageOneApi.Client.Models
 		public bool MultiCurrencyEnabled { get; set; }
 		[JsonProperty("use_live_exchange_rates")]
 		public bool UseLiveExchangeRates { get; set; }
+        [JsonProperty("base_currency_id")]
+        public string BaseCurrencyId { get; set; }
 		[JsonProperty("tax_scheme")]
 		public PropertyValueWithPath TaxScheme { get; set; }
 		[JsonProperty("tax_return_frequency")]
 		public PropertyValueWithPath TaxReturnFrequency { get; set; }
-		[JsonProperty("tax_number")]
+        [JsonProperty("mtd_activation_status")]
+        public string MtdActivationStatus { get; set; }
+        [JsonProperty("mtd_connected")]
+        public bool MtdConnected { get; set; }
+        [JsonProperty("mtd_authenticated_date")]
+        public DateTime? MtdAuthenticatedDate { get; set; }
+        [JsonProperty("tax_number")]
 		public string TaxNumber { get; set; }
 		[JsonProperty("general_tax_number")]
 		public string GeneralTaxNumber { get; set; }
