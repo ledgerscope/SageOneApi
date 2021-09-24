4b22470f7cf1c5fcb1d0fce98a76dbda56349622
Steve Aze <steve@ledgerscope.com>
2021-09-24 11:22:18 +0100

    For DateTime?, Deserialize "" as null rather than error.

--- SageOneApi.Client/JsonDeserializer.cs
+++ SageOneApi.Client/JsonDeserializer.cs
@@ -1,5 +1,7 @@
-﻿using SageOneApi.Client.Responses;
+﻿using System;
+using SageOneApi.Client.Responses;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace SageOneApi.Client
 {
@@ -10,14 +12,40 @@ namespace SageOneApi.Client
 #endif
 	static class JsonDeserializer
 	{
+		private static Lazy<JsonSerializerOptions> _lazyOptions = new Lazy<JsonSerializerOptions>(() => getOptions());
+
+		private static JsonSerializerOptions getOptions()
+		{
+			var options = new JsonSerializerOptions(JsonSerializerDefaults.General);
+			options.Converters.Add(new NullableDateTimeConverter());
+			return options;
+		}
+
 		public static T DeserializeObject<T>(string json)
 		{
-			return JsonSerializer.Deserialize<T>(json);
+			return JsonSerializer.Deserialize<T>(json, _lazyOptions.Value);
 		}
 
 		public static GetAllResponse<T> DeserializeObjects<T>(string json)
 		{
-			return JsonSerializer.Deserialize<GetAllResponse<T>>(json);
+			return JsonSerializer.Deserialize<GetAllResponse<T>>(json, _lazyOptions.Value);
+		}
+
+		private class NullableDateTimeConverter : JsonConverter<DateTime?>
+		{
+			public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			{
+				string sval = reader.GetString();
+				if (string.IsNullOrEmpty(sval))
+					return null;
+				else
+					return DateTime.Parse(sval);
+			}
+
+			public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+			{
+				writer.WriteStringValue(value.HasValue ? value.ToString() : null);
+			}
 		}
 	}
 }
