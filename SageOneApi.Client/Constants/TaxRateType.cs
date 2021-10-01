c7733c667bfcf5702ac66050a786ffb5972277a4
Matthew Steeples <matthew@ledgerscope.com>
2021-10-01 17:55:53 +0100

    Add new custom rate

--- SageOneApi.Client/Constants/TaxRateType.cs
+++ SageOneApi.Client/Constants/TaxRateType.cs
@@ -7,6 +7,11 @@
 		public const string Zero = "ZERO";
 		public const string Exempt = "EXEMPT";
 		public const string NoTax = "NO_TAX";
+
+		/// <summary>
+		/// 12.5% COVID-19 Rate
+		/// </summary>
+		public const string Reduced2 = "Reduced Rate 2 UK";
 	}
 }
 
