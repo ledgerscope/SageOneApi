31d7e36a04e0ef105a2566f2da52a711c5ab8b1d
Tomislav Barbaric <tomislav@ledgerscope.com>
2022-03-22 20:04:16 +0100

    Added Attachment model #3037

---
+++ SageOneApi.Client/Models/Attachment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace SageOneApi.Client.Models
+{
+    public class Attachment : SageOneAccountingEntity
+    {
+        [JsonPropertyName("deleted_at")]
+        public DateTime? DeletedAt { get; set; }
+        [JsonPropertyName("file")]
+        public string File { get; set; }
+        [JsonPropertyName("mime_type")]
+        public string MimeType { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        [JsonPropertyName("file_extension")]
+        public string FileExtension { get; set; }
+        [JsonPropertyName("transaction")]
+        public PropertyValueWithPath Transaction { get; set; }
+        [JsonPropertyName("file_size")]
+        public int FileSize { get; set; }
+        [JsonPropertyName("file_name")]
+        public string FileName { get; set; }
+        [JsonPropertyName("$file_path")]
+        public string FilePath { get; set; }
+        [JsonPropertyName("attachment_context_type")]
+        public PropertyValueWithPath AttachmentContextType { get; set; }
+        [JsonPropertyName("attachment_context")]
+        public PropertyValueWithPath AttachmentContext { get; set; }
+        [JsonPropertyName("is_public")]
+        public bool IsPublic { get; set; }
+    }
+}
