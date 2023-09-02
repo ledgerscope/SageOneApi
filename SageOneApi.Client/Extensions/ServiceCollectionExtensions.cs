a0659024147ffe2434ce547cec497408009ba5f5
Matthew Steeples <matthew@ledgerscope.com>
2023-09-02 13:40:55 +0100

    Add method to register DI classes

---
+++ SageOneApi.Client/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageOneApi.Client.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static ServiceCollection AddSageOneApi(this ServiceCollection services)
+        {
+            services.AddSingleton<SageOneApiClientConfig>((a) => SageOneApiClientConfig.Default); 
+            return services;
+        }
+    }
+}
