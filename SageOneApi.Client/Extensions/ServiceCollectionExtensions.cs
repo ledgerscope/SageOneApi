0dc0ce50b8da588c84b243540d050d9509b4bdf7
Matthew Steeples <matthew@ledgerscope.com>
2023-09-03 00:05:30 +0100

    Also register HttpClients

--- SageOneApi.Client/Extensions/ServiceCollectionExtensions.cs
+++ SageOneApi.Client/Extensions/ServiceCollectionExtensions.cs
@@ -9,9 +9,10 @@ namespace SageOneApi.Client.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static ServiceCollection AddSageOneApi(this ServiceCollection services)
+        public static T AddSageOneApi<T>(this T services) where T : IServiceCollection
         {
-            services.AddSingleton<SageOneApiClientConfig>((a) => SageOneApiClientConfig.Default); 
+            services.AddSingleton<SageOneApiClientConfig>((a) => SageOneApiClientConfig.Default);
+            services.AddHttpClient();
             return services;
         }
     }
